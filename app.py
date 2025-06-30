445dfa3bb8299181bd1518e09ee07b1e3116bf04
Akshay <amazingakshay005@gmail.com>
2025-06-30 19:46:54 +0530

    Add basic Flask app

---
+++ app.py
@@ -0,0 +1,11 @@
+# filepath: c:\Users\Lenovo\Desktop\mini-project-lpu-1\app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, Mini Project!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
